717239b45d1ba150bddde67a42afb12bab477152
Kim Rypstra <kim.rypstra@gmail.com>
2025-03-16 17:54:22 +0800

    Add font, fix gauge font, size, add alert background;

--- source/gauge/NumericGauge.py
+++ source/gauge/NumericGauge.py
@@ -1,8 +1,9 @@
 from kivy.properties import Property, StringProperty 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.graphics import Rectangle, Color
+from kivy.graphics import Rectangle
 
+from source.shared.Constants import COLOUR_RED, COLOUR_BLACK, GAUGE_FONT_SIZE
 
 from ..shared.DisposeBag import DisposeBag
 
@@ -14,18 +15,22 @@ class NumericGauge(BoxLayout):
 		self.view_model = NumericGaugeViewModel()
 		self.view_model.bind(value = self.update_canvas)
 
-		self.label = Label(text = 'initial')
+		self.label = Label(
+				text = 'initial', 
+				font_name = 'res/fonts/TitilliumWeb-Black.ttf',
+				font_size = GAUGE_FONT_SIZE
+			)
 		self.add_widget(self.label)
 
 	def update_canvas(self, value, something):
-		# self.label.text = something
+		self.label.text = something
 		with self.canvas.before:  # `before` ensures it’s drawn behind other elements
-			Color(0, 1, 0, 1)  # Green background (RGBA)
-			self.rect = Rectangle(pos=self.pos, size=self.size)
+			self.canvas.before.add(COLOUR_RED if self.view_model.alarm else COLOUR_BLACK)
+			self.rect = Rectangle(pos = self.pos, size = self.size)
 
 from kivy.event import EventDispatcher
 from kivy.clock import Clock
-from kivy.properties import Property, StringProperty 
+from kivy.properties import Property, StringProperty, BooleanProperty
 from source.shared.CANProvider import CANProvider 
 import reactivex as rx
 from reactivex import operators as ops
@@ -35,6 +40,7 @@ import threading
 class NumericGaugeViewModel(EventDispatcher):
 
 	value = StringProperty('')
+	alarm = BooleanProperty(False)
 
 	def __init__(self, **kwargs):
 		super(NumericGaugeViewModel, self).__init__(**kwargs)
@@ -46,10 +52,14 @@ class NumericGaugeViewModel(EventDispatcher):
 	def start_subscription(self):
 		self.subscription = self.can_provider.subscribe_to_pid(1) \
 			.subscribe(
-				on_next = lambda value: Clock.schedule_once(lambda dt: self.set_value(f'{value}'))
+				on_next = lambda value: Clock.schedule_once(lambda dt: self.set_value(value))
 			)
 
 		DisposeBag.shared().add(self.subscription)
 
 	def set_value(self, value):
-		self.value = value 
+		formatted = f'{value.value:.1f}'
+		self.value = formatted 
+
+		threshold = 0.5
+		self.alarm = value.value > threshold
--- source/shared/Constants.py
+++ source/shared/Constants.py
@@ -1,1 +1,7 @@
 GAUGE_SAMPLE_RATE = 0.1
+GAUGE_FONT_SIZE = 200
+
+from kivy.graphics import Color
+COLOUR_GREEN = Color(0, 1, 0, 1)
+COLOUR_RED = Color(1, 0, 0, 1)
+COLOUR_BLACK = Color(0, 0, 0, 1)
