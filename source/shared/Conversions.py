a734a40de68f3730f5a28834c5523fa1b9d22ff2
Kim Rypstra <kim.rypstra@gmail.com>
2025-05-31 16:17:53 +0800

    Class comment

--- source/shared/Conversions.py
+++ source/shared/Conversions.py
@@ -1,4 +1,4 @@
-# Conversions
+# Conversions from the value in a CAN frame to a human readable output
 
 CONVERSION_TEST = lambda can_frame: f'{can_frame.value:.1f}'
 CONVERSION_POS_NEG = lambda can_frame: 'positive' if can_frame.value > 0 else 'negative'
