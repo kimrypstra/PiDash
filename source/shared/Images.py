c4ec43a48f859be941e7a095360e40a11002b38b
Kim Rypstra <kim.rypstra@gmail.com>
2025-06-02 21:10:53 +0800

    Better button

---
+++ source/shared/Images.py
@@ -0,0 +1,5 @@
+# Images
+
+BUTTON_BACKGROUND = 'res/img/button_background.png'
+BUTTON_BACKGROUND_DOWN = 'res/img/button_background_down.png'
+BUTTON_BACKGROUND_SLICE = (50,50,50,50)
--- source/shared/views/DashButton.py
+++ source/shared/views/DashButton.py
@@ -1,18 +1,26 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.graphics import Color, RoundedRectangle
 
 from source.shared.Constants import BUTTON_HEIGHT
 from source.shared.Fonts import FONT_BOLD_ITALIC, FONT_SIZE_BUTTON
+from source.shared.Images import BUTTON_BACKGROUND, BUTTON_BACKGROUND_DOWN, BUTTON_BACKGROUND_SLICE
 
 
 class DashButton(BoxLayout):
 	def __init__(self, label, binding, **kwargs):
-		super(DashButton, self).__init__(padding=50, **kwargs)
+		super(DashButton, self).__init__(orientation='horizontal', **kwargs)
 
-		self.button = Button(text=label, size_hint_y=None, height=BUTTON_HEIGHT, font_name = FONT_BOLD_ITALIC, font_size = FONT_SIZE_BUTTON)
-		self.button.background_normal = ''
-		self.button.background_color = (0, 0, 0, 0)
-		self.button.bind(on_press=binding)
+		self.button = Button(text=label, size_hint_y=None, height=BUTTON_HEIGHT, font_name=FONT_BOLD_ITALIC, font_size=FONT_SIZE_BUTTON)
+		
+		self.button.background_normal = BUTTON_BACKGROUND
+		self.button.background_down = BUTTON_BACKGROUND_DOWN
+		self.button.border= BUTTON_BACKGROUND_SLICE
+		self.button.background_color = (1, 1, 1, 1)
 
+		self.button.bind(on_release=binding)
+
+		self.add_widget(Widget())
 		self.add_widget(self.button)
+		self.add_widget(Widget())
