5f5084a2b991ff0e3218dfddaeb0fa374489ef38
Kim Rypstra <kim.rypstra@gmail.com>
2025-06-08 16:55:08 +0800

    sample rate

--- source/shared/Constants.py
+++ source/shared/Constants.py
@@ -1,5 +1,5 @@
 # Gauge Constants
-GAUGE_SAMPLE_RATE = 1 # emissions per <x> seconds
+GAUGE_SAMPLE_RATE = 0.1 # emissions per <x> seconds
 THRESHOLD_TEST = 0.5
 
 BUTTON_HEIGHT = 100 # Note must contain button font size plus border thickness
